a516d46a2d0da43e2935e0de7391d3dd35cc47e5
gabrielxavier1825-bit <gabrielxavier1825@gmail.com>
2025-09-22 20:32:01 -0300

    exemplos

---
+++ ex2.py
@@ -0,0 +1,12 @@
+nome = "Gabriel"
+
+idade = 20
+
+# ola Gabriel voce tem 20 anos.
+print('ola',nome,',voce tem',idade,'anos.')
+
+# ola Gabriel, voce tem 20 anos.
+print('ola {nome}, voce tem {idade} anos.')
+
+# ola Gabriel, voce tem 20 anos.
+print('ola ' + nome + ', voce tem {idade} anos.')
