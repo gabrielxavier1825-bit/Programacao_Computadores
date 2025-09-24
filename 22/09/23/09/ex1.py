291d570f3e914ce7f1b0a3d24bfd0ea579b561e4
gabrielxavier1825-bit <gabrielxavier1825@gmail.com>
2025-09-23 22:00:48 -0300

    segunda aula

---
+++ 22/09/23/09/ex1.py
@@ -0,0 +1,11 @@
+ano_atual = 2025
+ano_nascimento = 2005
+
+idade =ano_atual-ano_nascimento
+
+#Voce tem 20 anos.
+print('voce tem', idade, 'anos.')
+
+print('voce tem' + str(idade) + 'anos.')
+
+print(f'voce tem {idade} anos.')
